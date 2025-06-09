b0f6e0b1c6044fcf0baf32d0f51ce589dff34378
assets72729 <hunxutia@gmail.com>
2025-06-10 01:39:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,12 +46,12 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeflixSupport")
-        self.LOGGER(__name__).info(f"""BOT DEPLOYED BY @CODEFLIX_BOTS""")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/KdramaTalkies")
+        self.LOGGER(__name__).info(f"""BOT DEPLOYED BY @KdramaTalkies""")
 
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @Codeflix_Bots")   
+        self.LOGGER(__name__).info(f"Bot Running..! Made by @KdramaTalkies")   
 
         # Start Web Server
         app = web.AppRunner(await web_server())
@@ -59,7 +59,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @KdramaTalkies</blockquote></b>")
         except: pass
 
     async def stop(self, *args):
